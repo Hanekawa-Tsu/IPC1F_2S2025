7dce1433703f3d30aa8fca282401e401f27a3548
Gustavo Chip <3119455460412@ingenieria.usac.edu.gt>
2025-08-30 19:07:05 -0600

    Texto

--- Proyecto1/Inventario.java
+++ Proyecto1/Inventario.java
@@ -2,4 +2,25 @@ import java.util.Scanner;
 
 public class Inventario {
     
+    //Menu de opciones
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        int opcion;
+
+        do{
+            System.out.println("Menu de Gestor de Productos");
+            System.out.println("Selecciones una Opcion ingresando el Numero");
+            System.out.println("1-Agregar Procducto");
+            System.out.println("2-Buscar Producto");
+            System.out.println("3-Eliminar Procducto");
+            System.out.println("4-Registrar Venta");
+            System.out.println("5-Generar Reporte");
+            System.out.println("6-Ver Datos de Estudiante");
+            System.out.println("7-Bitacora");
+            System.out.println("8-Salir");
+
+            
+    }
 }
