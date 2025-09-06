87dee6ead3231ca7c7565ddf05ad4fff53a7b9de
Gustavo Chip <3119455460412@ingenieria.usac.edu.gt>
2025-09-05 21:47:33 -0600

    estructura de agregar producto (incompleto)

--- Proyecto1/Inventario.java
+++ Proyecto1/Inventario.java
@@ -12,9 +12,9 @@ public class Inventario {
         do{
             System.out.println("Menu de Gestor de Productos");
             System.out.println("Selecciones una Opcion ingresando el Numero");
-            System.out.println("1-Agregar Procducto");
+            System.out.println("1-Agregar Producto");
             System.out.println("2-Buscar Producto");
-            System.out.println("3-Eliminar Procducto");
+            System.out.println("3-Eliminar Producto");
             System.out.println("4-Registrar Venta");
             System.out.println("5-Generar Reporte");
             System.out.println("6-Ver Datos de Estudiante");
@@ -26,26 +26,32 @@ public class Inventario {
                 
                 switch (opcion) {
                     case 1:
-                        
-                        break;
+                        Aproducto(scanner); //Aproducto = Agregar producto
+                       
                     case 2:
-                        break;
+                        Bproducto(scanner); //Bprodcuto = Buscar producto
+                    
                     case 3:
-                        break;
+                        Eproducto(scanner); // Eproducto = Eliminar producto
+                        
                     case 4:
-                        break;
+                        Rventa(scanner); // Rventa = Registrar venta
+                        
                     case 5:
-                        break;
+                        Greporte(scanner); // Greporte = Generar reporte
+                        
                     case 6:
-                        break;
+                        Vestudiante(scanner); // Vestudiante = Ver datos de estudiante
+                        
                     case 7:
-                        break;
+                        Bitacora(scanner); // Bitacora
+                       
                     case 8:
-                        System.out.println("Sesion Finalizada");
+                        System.out.println("Sesion Finalizada"); //Fin
                         break;
                     default:
-                        System.out.println("Opcion invalida");
-                        break;
+                        System.out.println("Opcion invalida"); // Error
+                        
                 }
             }catch (NumberFormatException e){
                     System.out.println("Error");
@@ -55,4 +61,65 @@ public class Inventario {
         }while (opcion !=8);
         scanner.close();
     }
+
+    // Agregar producto
+    public static void Aproducto(Scanner scanner){
+
+        do{
+            System.out.println("Nombre del producto: ");
+            String nombre = scanner.nextLine();
+
+            System.out.println("Categoria: ");
+            String categoria = scanner.nextLine();
+
+            System.out.println("Precio: ");
+            Float precio = scanner.nextFloat(); //Al ingresar decimales usa "," (la coma) el punto puede dar error
+        
+            System.out.println("Cantidad de Stock");
+            int cantidad = scanner.nextInt();
+
+            System.out.println("Codigo de 9 digitos");
+            int codigo = scanner.nextInt();
+
+            int opcion2; //Pendiente de cambiar se agregros solo para probar el codigo
+            opcion2 = Integer.parseInt(scanner.nextLine());
+            switch (opcion2) {
+                case 1:
+                    
+                    break;
+            
+                default:
+                    break;
+            }while(opcion2 !=2);
+         scanner.close();
+
+        }
+    }
+
+    // Puede cambiar **no tocar**
+    public static void Bproducto(Scanner scanner){
+
+    }
+
+    public static void Eproducto(Scanner scanner) {
+        
+    }
+
+    public static void Rventa(Scanner scanner) {
+        
+    }
+
+    public static void Greporte(Scanner scanner) {
+        
+    }
+
+    public static void Vestudiante(Scanner scanner) {
+        
+    }
+
+    public static void Bitacora(Scanner scanner) {
+        
+    }
+
+
 }
