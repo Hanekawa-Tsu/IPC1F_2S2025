f5088f7a4aee7af02674fbf57662142ca5f574b2
Gustavo Chip <3119455460412@ingenieria.usac.edu.gt>
2025-09-15 13:15:07 -0600

    actividad 6

---
+++ IPC1_Ejercicios_202308450/Unidad 1/Ejercicio de pdf con matriz/Actividad6.java
@@ -0,0 +1,69 @@
+import com.itexpdf.text.Document;
+import com.itexpdf.text.DocumentException;
+import com.itexpdf.text.pdf.PdfPTable;
+import com.itexpdf.text.pdf.PdfWriter;
+import com.itexpdf.text.Paragraph;
+import java.io.FileOutputStream;
+import java.io.File;
+
+public class Actividad6 {
+
+    private String crearPDF;
+    
+    public Actividad6(String archivo) {
+        //Ruta donde se guardara el PDF
+        this.crearPDF = System.getProperty("user.home") + "/Desktop/" + archivo;
+    }
+
+    public void paises(String titulo){ //Metodo para agregar los paises
+        Document documento = new Document(); //Crear el documento
+        
+        try{
+            PdfWriter.getInstance(documento, new FileOutputStream(this.crearPDF)); //Crear el PDF
+            documento.open(); //Abrir el documento
+
+            documento.add (new Paragraph(titulo)); //Titulo del documento
+            documento.add (new Paragraph("\n "));
+
+            PdfPTable tabla = new PdfPTable(3);
+            tabla.addCell("Pais"); // addCell peromite agregar texto en las columnas
+            tabla.addCell("Capital");
+            tabla.addCell("Poblacion");
+
+            tabla.addCell("Guatemala");
+            tabla.addCell("Ciudad de Guatemala");
+            tabla.addCell("18 millones");
+
+            tabla.addCell("Mexico");
+            tabla.addCell("Ciudad de Mexico");
+            tabla.addCell("126 millones");
+
+            tabla.addCell("España");
+            tabla.addCell("Madrid");
+            tabla.addCell("47 millones");
+
+            documento.add(tabla); //Agregar la tabla al documento
+            
+            System.out.println("PDF creado exitosamente en: " + this.crearPDF);
+        
+        } catch (DocumentException | java.oi.FileNotFoundException e){
+            e.printStackTrace();
+        } finally {
+            if (documento.isOpen()) {
+                documento.close(); //Cerrar el documento
+            }
+        }
+    }
+    
+}
+
+class GeneradorDePDF {
+
+    public static void main(String[] args) {
+        // 5. Instanciar un objeto de la clase GeneradorDePDF
+        Actividad6 miGenerador = new Actividad6("Actividad6.pdf");
+        
+        // 6. Llamar a un método del objeto para ejecutar la acción
+        miGenerador.paises("Reporte de Países y Capitales");
+    }
+}
