5ff9e97973fbdb11991a23460077be41e3653dc8
Gustavo Chip <3119455460412@ingenieria.usac.edu.gt>
2025-09-27 20:56:50 -0600

    importacion de librerias

--- Practica 2/Juego_Por_Turnos.java
+++ Practica 2/Juego_Por_Turnos.java
@@ -1,3 +1,8 @@
-public class Juego_Por_Turnos {
+import javax.swing.*; // Importar librerías Swing
+import java.awt.*; // Importar librerías AWT
+import java.awt.event.*; // Importar librerías de eventos
+
+public class Juego_Por_Turnos extends JFrame {
     
+
 }
