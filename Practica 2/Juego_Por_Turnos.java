94f33896e508d3d0ce7f43672c71fdd69ee6e66b
Gustavo Chip <3119455460412@ingenieria.usac.edu.gt>
2025-09-27 21:05:31 -0600

    Creacion de clase para estadisticas de pokemon

--- Practica 2/Juego_Por_Turnos.java
+++ Practica 2/Juego_Por_Turnos.java
@@ -4,5 +4,16 @@ import java.awt.event.*; // Importar librerías de eventos
 
 public class Juego_Por_Turnos extends JFrame {
     
+// Clase Pokémon
+class Pokemon {
+    String nombre;
+    int hp, atk, def;
 
+    public Pokemon(String nombre, int hp, int atk, int def) {
+        this.nombre = nombre;
+        this.hp = hp;
+        this.atk = atk;
+        this.def = def;
+        }
+    }
 }
